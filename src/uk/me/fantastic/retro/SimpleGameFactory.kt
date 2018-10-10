d7b20781e2cf6e24c4d60fc1ada1a0b2666a7580
Richard Smith <mercurial@fantastic.me.uk>
2018-10-10 21:14:15 +0100

    buttonmasher is now the groovy version, not the java version.  and snakeeaters added.

--- src/uk/me/fantastic/retro/SimpleGameFactory.kt
+++ src/uk/me/fantastic/retro/SimpleGameFactory.kt
@@ -4,11 +4,15 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import uk.me.fantastic.retro.screens.GameSession
 
-class SimpleGameFactory(name: String, private val gameClazz: Class<out Game>) : AbstractGameFactory(name = name) {
+class SimpleGameFactory(name: String, private val gameClazz: Class<out Game>, path: String, override val image:
+Texture = Resources.MISSING_TEXTURE) :
+        AbstractGameFactory
+(name = name, pathPrefix = path) {
 
     override val description = name
 
+
     override fun create(session: GameSession): Game {
-        return gameClazz.getConstructor(GameSession::class.java).newInstance(session)
+        return gameClazz.getConstructor(GameSession::class.java, String::class.java).newInstance(session, pathPrefix)
     }
 }
