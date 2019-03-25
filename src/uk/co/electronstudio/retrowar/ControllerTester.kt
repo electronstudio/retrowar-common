45f48cc1b43debc552b5925c2b735c69764619c2
electronstudio <mercurial@fantastic.me.uk>
2019-03-25 18:39:03 +0000

    submodules fix

--- src/uk/co/electronstudio/retrowar/ControllerTester.kt
+++ src/uk/co/electronstudio/retrowar/ControllerTester.kt
@@ -1,6 +1,7 @@
 package uk.co.electronstudio.retrowar
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.controllers.Controller
 
 import com.badlogic.gdx.controllers.PovDirection
 import com.badlogic.gdx.graphics.GL20
@@ -27,89 +28,100 @@ class ControllerTester(session: GameSession) : SimpleGame(session, 640f, 480f, f
         //  batch.begin()
 
         var x = 0f
-        for (i in 0..App.app.controllers.getControllers().size - 1) {
-            var y = 472f
+        var y = 472f
+        for (i in 0..App.app.controllers.getControllers().size/2 - 1) {
             val m = App.app.controllers.getControllers()[i]
-            //  val c = m.controller
             font.draw(batch, m.name, x, y, 256f, Align.left, false)
-            y -= 8
-            // font.draw(batch, m.mapping, x, y, 256f, Align.left, false)
-            for (j in 0..31) {
-                if (m.getButton(j)) {
-                    y -= 8f
-                    val mapping: String = when (j) {
-                        SDL.SDL_CONTROLLER_BUTTON_A -> "A"
-                        SDL.SDL_CONTROLLER_BUTTON_B -> "B"
-                        SDL.SDL_CONTROLLER_BUTTON_X -> "X"
-                        SDL.SDL_CONTROLLER_BUTTON_Y -> "Y"
-                        SDL.SDL_CONTROLLER_BUTTON_LEFTSHOULDER -> "L_BUMPER"
-                        SDL.SDL_CONTROLLER_BUTTON_RIGHTSHOULDER -> "R_BUMPER"
-                        SDL.SDL_CONTROLLER_BUTTON_GUIDE -> "GUIDE"
-                        SDL.SDL_CONTROLLER_BUTTON_BACK -> "BACK"
-                        SDL.SDL_CONTROLLER_BUTTON_START -> "START"
-                        SDL.SDL_CONTROLLER_BUTTON_DPAD_DOWN -> "DPAD_DOWN"
-                        SDL.SDL_CONTROLLER_BUTTON_DPAD_UP -> "DPAD_UP"
-                        SDL.SDL_CONTROLLER_BUTTON_DPAD_LEFT -> "DPAD_LEFT"
-                        SDL.SDL_CONTROLLER_BUTTON_DPAD_RIGHT -> "DPAD_RIGHT"
-                        //     SDL.SDL_CONTROLLER_BUTTON_  -> "L_TRIGGER"
-                        //    SDL.SDL_CONTROLLER_BUTTON_A  -> "R_TRIGGER"
-                        SDL.SDL_CONTROLLER_BUTTON_RIGHTSTICK -> "R_STICK_PUSH"
-                        SDL.SDL_CONTROLLER_BUTTON_LEFTSTICK -> "L_STICK_PUSH"
-                        else -> "UNMAPPED BUTTON $j"
-                    }
-                    font.draw(batch, mapping, x, y, 256f, Align.left, false)
-                }
-            }
-
-            for (j in 0..31) {
-                if (m.getAxis(j) != 0f) {
-                    y -= 8f
-                    val mapping: String = when (j) {
-                        SDL.SDL_CONTROLLER_AXIS_LEFTX -> "L_STICK_HORIZONTAL_AXIS"
-                        SDL.SDL_CONTROLLER_AXIS_RIGHTX -> "R_STICK_HORIZONTAL_AXIS"
-                        SDL.SDL_CONTROLLER_AXIS_LEFTY -> "L_STICK_VERTICAL_AXIS"
-                        SDL.SDL_CONTROLLER_AXIS_RIGHTY -> "R_STICK_VERTICAL_AXIS"
-                        SDL.SDL_CONTROLLER_AXIS_TRIGGERLEFT -> "L_TRIGGER_AXIS"
-                        SDL.SDL_CONTROLLER_AXIS_TRIGGERRIGHT -> "R_TRIGGER_AXIS"
-                        else -> "UNMAPPED AXIS $j"
-                    }
-                    font.draw(batch, "$mapping: ${m.getAxis(j)} ", x, y, 256f, Align.left, false)
-                }
-            }
-            for (j in 0..31) {
-                if (m.getPov(j) != PovDirection.center) {
-                    y -= 8f
-                    val mapping: String = when (j) {
-                        // m.DPAD -> "DPAD"
-                        else -> "UNKNOWN DPAD $j"
-                    }
-                    font.draw(batch, "$mapping ${m.getPov(j)}", x, y, 256f, Align.left, false)
-                }
-            }
-            //            try {
-            //                for (j in 0..31) {
-            //                    y -= 8f
-            //                    font.draw(batch, "Accel$j: ${c.getAccelerometer(j)}", x, y, 256f, Align.left, false)
-            //                }
-            //            } catch (e: GdxRuntimeException) {
-            //            }
-            //
-            //            for (j in 0..31) {
-            //                if (c.getSliderX(j)) {
-            //                    y -= 8f
-            //                    font.draw(batch, "XSlider$j: ${c.getSliderX(j)}", x, y, 256f, Align.left, false)
-            //                }
-            //            }
-            //            for (j in 0..31) {
-            //                if (c.getSliderY(j)) {
-            //                    y -= 8f
-            //                    font.draw(batch, "YSlider$j: ${c.getSliderY(j)}", x, y, 256f, Align.left, false)
-            //                }
-            //            }
-            x += 256f
+            drawController(m, y, batch, x)
+            y -= 50
+        }
+        x=320f
+        y=0f
+        for (i in App.app.controllers.getControllers().size/2 ..  App.app.controllers.getControllers().size-1 ) {
+            val m = App.app.controllers.getControllers()[i]
+            font.draw(batch, m.name, x, y, 256f, Align.left, false)
+            drawController(m, y, batch, x)
+            y -= 50
         }
 
         //  batch.end()
+    }
+
+    private fun drawController(m: Controller, y: Float, batch: Batch, x: Float): Float {
+        var y1 = y
+        for (j in 0..31) {
+            if (m.getButton(j)) {
+                y1 -= 8f
+                val mapping: String = when (j) {
+                    SDL.SDL_CONTROLLER_BUTTON_A -> "A"
+                    SDL.SDL_CONTROLLER_BUTTON_B -> "B"
+                    SDL.SDL_CONTROLLER_BUTTON_X -> "X"
+                    SDL.SDL_CONTROLLER_BUTTON_Y -> "Y"
+                    SDL.SDL_CONTROLLER_BUTTON_LEFTSHOULDER -> "L_BUMPER"
+                    SDL.SDL_CONTROLLER_BUTTON_RIGHTSHOULDER -> "R_BUMPER"
+                    SDL.SDL_CONTROLLER_BUTTON_GUIDE -> "GUIDE"
+                    SDL.SDL_CONTROLLER_BUTTON_BACK -> "BACK"
+                    SDL.SDL_CONTROLLER_BUTTON_START -> "START"
+                    SDL.SDL_CONTROLLER_BUTTON_DPAD_DOWN -> "DPAD_DOWN"
+                    SDL.SDL_CONTROLLER_BUTTON_DPAD_UP -> "DPAD_UP"
+                    SDL.SDL_CONTROLLER_BUTTON_DPAD_LEFT -> "DPAD_LEFT"
+                    SDL.SDL_CONTROLLER_BUTTON_DPAD_RIGHT -> "DPAD_RIGHT"
+                    //     SDL.SDL_CONTROLLER_BUTTON_  -> "L_TRIGGER"
+                    //    SDL.SDL_CONTROLLER_BUTTON_A  -> "R_TRIGGER"
+                    SDL.SDL_CONTROLLER_BUTTON_RIGHTSTICK -> "R_STICK_PUSH"
+                    SDL.SDL_CONTROLLER_BUTTON_LEFTSTICK -> "L_STICK_PUSH"
+                    else -> "UNMAPPED BUTTON $j"
+                }
+                font.draw(batch, mapping, x, y1, 256f, Align.left, false)
+            }
+        }
+
+        for (j in 0..31) {
+            if (m.getAxis(j) != 0f) {
+                y1 -= 8f
+                val mapping: String = when (j) {
+                    SDL.SDL_CONTROLLER_AXIS_LEFTX -> "L_STICK_HORIZONTAL_AXIS"
+                    SDL.SDL_CONTROLLER_AXIS_RIGHTX -> "R_STICK_HORIZONTAL_AXIS"
+                    SDL.SDL_CONTROLLER_AXIS_LEFTY -> "L_STICK_VERTICAL_AXIS"
+                    SDL.SDL_CONTROLLER_AXIS_RIGHTY -> "R_STICK_VERTICAL_AXIS"
+                    SDL.SDL_CONTROLLER_AXIS_TRIGGERLEFT -> "L_TRIGGER_AXIS"
+                    SDL.SDL_CONTROLLER_AXIS_TRIGGERRIGHT -> "R_TRIGGER_AXIS"
+                    else -> "UNMAPPED AXIS $j"
+                }
+                font.draw(batch, "$mapping: ${m.getAxis(j)} ", x, y1, 256f, Align.left, false)
+            }
+        }
+        for (j in 0..31) {
+            if (m.getPov(j) != PovDirection.center) {
+                y1 -= 8f
+                val mapping: String = when (j) {
+                    // m.DPAD -> "DPAD"
+                    else -> "UNKNOWN DPAD $j"
+                }
+                font.draw(batch, "$mapping ${m.getPov(j)}", x, y1, 256f, Align.left, false)
+            }
+        }
+        //            try {
+        //                for (j in 0..31) {
+        //                    y -= 8f
+        //                    font.draw(batch, "Accel$j: ${c.getAccelerometer(j)}", x, y, 256f, Align.left, false)
+        //                }
+        //            } catch (e: GdxRuntimeException) {
+        //            }
+        //
+        //            for (j in 0..31) {
+        //                if (c.getSliderX(j)) {
+        //                    y -= 8f
+        //                    font.draw(batch, "XSlider$j: ${c.getSliderX(j)}", x, y, 256f, Align.left, false)
+        //                }
+        //            }
+        //            for (j in 0..31) {
+        //                if (c.getSliderY(j)) {
+        //                    y -= 8f
+        //                    font.draw(batch, "YSlider$j: ${c.getSliderY(j)}", x, y, 256f, Align.left, false)
+        //                }
+        //            }
+        return y1
     }
 
     // These methods must be implemented but don't have to do anything
