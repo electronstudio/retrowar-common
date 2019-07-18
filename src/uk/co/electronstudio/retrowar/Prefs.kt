c4c124dd42f6609cb16fe996df42af7a54b31768
Richard Smith <mercurial@fantastic.me.uk>
2019-07-18 12:13:31 +0100

    change parsec menus

--- src/uk/co/electronstudio/retrowar/Prefs.kt
+++ src/uk/co/electronstudio/retrowar/Prefs.kt
@@ -51,6 +51,7 @@ object Prefs {
 
     /** @suppress */
     enum class MultiChoicePref(val pref: String, vararg val choices: String, val default: Int = 0) {
+        SERVER_MODE("serverMode","COMPATIBLE (DESKTOP CAPTURE)", "FULL INTEGRATION (EXPERIMENTAL"),
         PARTICLES("particles", "ON", "OFF", "EXCESSIVE"),
         RUMBLE("rumble", "HIGH", "LOW", "OFF"),
         INPUT("input", "RAW INPUT", "XINPUT", "DIRECT INPUT"),
