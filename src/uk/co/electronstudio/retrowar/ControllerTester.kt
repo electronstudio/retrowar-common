1a1527d4c46ba7c4cae5ac7299288a65969d469d
electronstudio <misc@fantastic.me.uk>
2019-12-09 12:14:28 +0000

    controller test screen includes mobcontrol

--- src/uk/co/electronstudio/retrowar/ControllerTester.kt
+++ src/uk/co/electronstudio/retrowar/ControllerTester.kt
@@ -58,11 +58,11 @@ class ControllerTester(session: GameSession) : SimpleGame(session, 640f, 360f, f
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f) // clear the screen
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        font.draw(batch, "Controllers connected: ${Controllers.getControllers().size} + ${App.app.networkControllers.size}", 0f, 20f)
+        font.draw(batch, "Local controllers : ${Controllers.getControllers().size}    Network controllers: ${App.app.networkControllers.size}     Mobile controllers: ${App.app.mobControllerManager.controllers.size}", 0f, 20f)
 
         var y = height
 
-        val controllers = Controllers.getControllers() + App.app.networkControllers.values
+        val controllers = Controllers.getControllers() + App.app.networkControllers.values + App.app.mobControllerManager.controllers
 
         controllers.forEachIndexed { index, controller ->
             val x = if (index % 2 == 0) 0f else width / 2f
